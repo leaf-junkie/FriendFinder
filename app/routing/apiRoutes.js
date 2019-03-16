2a1d0900f6cf4d67f73c1a37f6bba5f51d53f40d
leaf-junkie <ragsdale.jar@gmail.com>
2019-03-16 17:03:08 -0400

    add matchmaker logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,13 @@
-// Your apiRoutes.js file should contain two routes:
-
 // A GET route with the url /api/friends. This will be used to display a 
 // JSON of all possible friends.
+app.get('/friends', function(req, res) {
+    const friends = req.params.friends;
+});
 
 // A POST routes /api/friends. This will be used to handle incoming survey 
 // results. This route will also be used to handle the compatibility logic.
+app.post('/', function(req, res) {
 
-app.get('/', ) {
-    /api/friends
-}
+});
 
-app.post('/', ) {
-    /api/friends
-}
+res.send(result.toString());
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-// Your server.js file should require the basic 
-// npm packages we've used in class: express and path.
-
 const foundation = require('foundation');
 const express = require('express');
 const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 // change whatever your path is - allows you to use images and custom css on heroku
 app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+connection.connect((err) => {
+    if (err) {
+      console.error(`Error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`Connected as id: ${connection.threadId}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
